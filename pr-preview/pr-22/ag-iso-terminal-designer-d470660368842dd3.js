611695c5d32726db3e79a4bef4adb3e1b0eef2d2
GwnDaan <GwnDaan@users.noreply.github.com>
2025-11-19 20:09:48 +0000

    Deploy preview for PR 22 🛫

--- pr-preview/pr-22/ag-iso-terminal-designer-d470660368842dd3.js
+++ pr-preview/pr-22/ag-iso-terminal-designer-d470660368842dd3.js
@@ -297,17 +297,6 @@ function getArrayI8FromWasm0(ptr, len) {
     ptr = ptr >>> 0;
     return getInt8ArrayMemory0().subarray(ptr / 1, ptr / 1 + len);
 }
-function wasm_bindgen__convert__closures_____invoke__hc65c942d5980b8c2(arg0, arg1) {
-    wasm.wasm_bindgen__convert__closures_____invoke__hc65c942d5980b8c2(arg0, arg1);
-}
-
-function wasm_bindgen__convert__closures_____invoke__h078f99eab5f860b9(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__h078f99eab5f860b9(arg0, arg1, arg2);
-}
-
-function wasm_bindgen__convert__closures_____invoke__hff9c13d9576d72a0(arg0, arg1, arg2) {
-    wasm.wasm_bindgen__convert__closures_____invoke__hff9c13d9576d72a0(arg0, arg1, arg2);
-}
 
 function takeFromExternrefTable0(idx) {
     const value = wasm.__wbindgen_externrefs.get(idx);
@@ -319,6 +308,18 @@ function wasm_bindgen__convert__closures_____invoke__hc59c42ddc9c053e8(arg0, arg
     if (ret[1]) {
         throw takeFromExternrefTable0(ret[0]);
     }
+}
+
+function wasm_bindgen__convert__closures_____invoke__h078f99eab5f860b9(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__h078f99eab5f860b9(arg0, arg1, arg2);
+}
+
+function wasm_bindgen__convert__closures_____invoke__hc65c942d5980b8c2(arg0, arg1) {
+    wasm.wasm_bindgen__convert__closures_____invoke__hc65c942d5980b8c2(arg0, arg1);
+}
+
+function wasm_bindgen__convert__closures_____invoke__hff9c13d9576d72a0(arg0, arg1, arg2) {
+    wasm.wasm_bindgen__convert__closures_____invoke__hff9c13d9576d72a0(arg0, arg1, arg2);
 }
 
 function wasm_bindgen__convert__closures_____invoke__h4fba6566125b6c9b(arg0, arg1, arg2, arg3) {
